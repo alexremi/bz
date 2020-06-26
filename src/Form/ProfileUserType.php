0078b51e09d8ade145cae46bc712ce0a29400fff
polina chagina <chagina.pa@gmail.com>
2020-06-26 16:59:16 +1000

    fix wordings

--- src/Form/ProfileUserType.php
+++ src/Form/ProfileUserType.php
@@ -16,12 +16,12 @@ class ProfileUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class)
+            ->add('username', TextType::class, array( 'label' => 'Имя' ))
             ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options' => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options' => array('label' => 'Пароль'),
+                'second_options' => array('label' => 'Повторите пароль'),
             ))
             ;
     }
