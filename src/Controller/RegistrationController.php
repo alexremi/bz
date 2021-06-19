a11fe69f39fcb090a025bf66a8c93b9fde7f45f6
Ruslan Khaliullin <khaliullin@taptima.ru>
2021-06-20 00:27:53 +1000

    Add email registration notification

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Form\ProfileUserType;
 use App\Entity\User;
+use App\Mail\NotificationMailManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,29 +12,40 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class RegistrationController extends Controller
 {
     /**
+     * @var NotificationMailManager
+     */
+    private NotificationMailManager $mailManager;
+
+    /**
+     * @param NotificationMailManager $mailManager
+     */
+    public function __construct(NotificationMailManager $mailManager)
+    {
+        $this->mailManager = $mailManager;
+    }
+
+    /**
      * @Route("/register", name="user_registration")
      */
     public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-        // 1) постройте форму
         $user = new User();
         $form = $this->createForm(ProfileUserType::class, $user);
 
-        // 2) обработайте отправку (произойдёт только в POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
-            // 3) Зашифруйте пароль (вы также можете сделать это через слушатель Doctrine)
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
 
-            // 4) сохраните Пользователя!
+            $context = [
+                'url' => $request->getSchemeAndHttpHost() . $this->generateUrl('login'),
+            ];
+
+            $this->mailManager->sendRegistrationNotification($user, $context);
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-
-            // ... сделайте любую другую работу - вроде отправки письма и др
-            // может, установите "флеш" сообщение об успешном выполнении для пользователя
 
             return $this->redirectToRoute('login');
         }
